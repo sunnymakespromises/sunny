6ea5e9d3d8d42b8232e283e9325200fe35135dd7
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-21 03:20:58 -0400

    removed stupid dock stuff

--- components/app.js
+++ components/app.js
@@ -7,9 +7,8 @@ import { useEffect, useState } from 'react'
 import { useHomeContext } from '../contexts/home'
 
 /* /components/app.js */
-export default function App({ id, title, logo, isDocked, hovered, setHovered, cursorPos }) {
+export default function App({ id, title, logo, isDocked, hovered, setHovered}) {
     const { onAppClick, isLandscape, opened } = useHomeContext()
-    const [scale, setScale] = useState()
     let [hoverRef, isHovered] = useHover()
     useEffect(() => {
         if (isLandscape) {
@@ -22,33 +21,15 @@ export default function App({ id, title, logo, isDocked, hovered, setHovered, cu
         }
     }, [isHovered])
 
-    useEffect(() => {
-        if (isLandscape) {
-            if (cursorPos) {
-                let dockContainer = document.getElementById('dock-container')
-                let dock = {left: dockContainer.offsetLeft, right: dockContainer.offsetLeft + dockContainer.clientWidth}
-                let cursorPercentageAcrossDock = ((cursorPos.x - dock.left) / (dock.right - dock.left))
-                let appPercentageAcrossDock = (((hoverRef.current.getBoundingClientRect().left + (hoverRef.current.getBoundingClientRect().width / 2)) - dock.left) / (dock.right - dock.left))
-                let distanceToCursor = (cursorPercentageAcrossDock - appPercentageAcrossDock) * 20
-                let x = distanceToCursor
-                let scaleFunction = 1 + ((0.15) * (Math.E ** (-1*((x**2)/2))))
-                setScale(scaleFunction)
-            }
-            else {
-                setScale(1)
-            }
-        }
-    }, [cursorPos])
-
     if (isLandscape) {
         return (
-            <div ref = {hoverRef} className = 'app-container flex flex-col items-center h-full aspect-square p-3 pb-0 gap-2 mb-1' onClick = {() => onAppClick(id)}>
+            <div ref = {hoverRef} className = 'app-container relative flex flex-col items-center h-full aspect-square p-3 pb-0 gap-2 mb-1' onClick = {() => onAppClick(id)}>
             {hovered === id ? 
                 <div className = 'app-title-container absolute bottom-[110%] text-center px-2 rounded-md backdrop-blur-3xl backdrop-brightness-[1.4] dark:backdrop-brightness-[0.8] border-[1px] border-white border-opacity-25'>
                     <Text style = 'appTitle' classNames = 'app-title h-min select-none'>{title}</Text>
                 </div>
             :null}
-                <Image styles = {{transform: 'scale(' + scale + ')'}} path = {logo} classNames = 'app-logo h-full aspect-square rounded-2xl cursor-pointer origin-bottom shadow-app'/>
+                <Image path = {logo} classNames = 'app-logo h-full aspect-square rounded-2xl cursor-pointer origin-bottom shadow-app'/>
                 <div className = {'transition-all app-opened-indicator h-1 aspect-square rounded-full bg-white ' + (opened.includes(id) ? 'bg-opacity-50' : 'bg-opacity-0')}/>
             </div>
         )
--- components/dock.js
+++ components/dock.js
@@ -6,38 +6,20 @@ import { useHomeContext } from '../contexts/home'
 import Link from 'next/link'
 
 /* /components/dock.js */
-export default function Dock({ children }) {
+export default function Dock() {
     const { pages, isHome, isLandscape, onAppClick } = useHomeContext()
     const [hovered, setHovered] = useState(null)
-    const [cursorPos, setCursorPos] = useState(null)
-    const dockContainerRef = useRef()
-    useEffect(() => { // cursor tracking for carousel effect
-        if (isLandscape) {
-            const handleMouseMove = (event) => {
-                if (dockContainerRef && dockContainerRef.current.contains(event.target)) {
-                    setCursorPos({ x: event.clientX, y: event.clientY })
-                }
-                else {
-                    setCursorPos(null)
-                }
-            }
-            window.addEventListener('mousemove', handleMouseMove)
-            return () => {
-                window.removeEventListener('mousemove', handleMouseMove)
-            }
-        }
-    }, [hovered, isLandscape])
 
     if (isHome || !isHome && isLandscape) {
         return (
-            <div ref = {dockContainerRef} id = 'dock-container' className = 'flex flex-row items-center justify-center shrink-0 gap-2 w-[90%] md:w-min h-[7.5rem] md:h-28 mb-[5%] md:mb-[1%] rounded-3xl backdrop-blur-dock backdrop-brightness-[1.4] dark:backdrop-brightness-[0.8] md:border-[1px] md:border-white md:border-opacity-25 z-10'>
+            <div id = 'dock-container' className = 'flex flex-row items-center justify-center shrink-0 gap-2 w-[90%] md:w-min h-[7.5rem] md:h-28 mb-[5%] md:mb-[1%] rounded-3xl backdrop-blur-dock backdrop-brightness-[1.4] dark:backdrop-brightness-[0.8] md:border-[1px] md:border-white md:border-opacity-25 z-10'>
                 {isLandscape ? pages.map((page, index) => {
                     if (page.visible.desktop === true) {
-                        return <App key = {index} id = {page.id} title = {page.title} logo = {page.logo} isDocked = {true} hovered = {hovered} setHovered = {setHovered} cursorPos = {cursorPos}/>
+                        return <App key = {index} id = {page.id} title = {page.title} logo = {page.logo} isDocked = {true} hovered = {hovered} setHovered = {setHovered}/>
                     }
                 }) : pages.map((page, index) => {
                     if (page.mobileDocked === true && page.visible.mobile === true) {
-                        return <App key = {index} id = {page.id} title = {page.title} logo = {page.logo} isDocked = {true} hovered = {hovered} setHovered = {setHovered} cursorPos = {cursorPos}/>
+                        return <App key = {index} id = {page.id} title = {page.title} logo = {page.logo} isDocked = {true} hovered = {hovered} setHovered = {setHovered}/>
                     }
                 })}
             </div>
